b98fa5f4f41cd46879a1f921d70b5545266fb995
Desmond <desmond.bera@gmail.com>
2018-07-02 23:14:23 -0400

    set up JWT authentication

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://desmond:desmond1@ds125031.mlab.com:25031/devconnectorapp'
+  mongoURI: 'mongodb://desmond:desmond1@ds125031.mlab.com:25031/devconnectorapp',
+  secretOrKey: 'secret'
 }
