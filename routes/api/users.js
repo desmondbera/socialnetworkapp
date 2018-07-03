55470e6a8ec4c613772529f226f6d10590e1c678
Desmond <desmond.bera@gmail.com>
2018-07-03 17:48:17 -0400

    added Passport/JWT authentication logic

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,7 +4,7 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
-
+const passport = require('passport');
 
 //Load User model
 const User = require('../../models/User');
@@ -83,7 +83,8 @@ router.post('/login', (req, res) => {
                 success: true,
                 token: 'Bearer ' + token
               });
-            });
+            }
+          );
           } else {
             return res.status(400).json({password: 'Password incorrect!'});
           }
@@ -91,5 +92,19 @@ router.post('/login', (req, res) => {
     });
 });
 
+// @route Get api/user/current
+// @desc Return current user
+// @access Private
+router.get(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
