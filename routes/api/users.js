08e1e7f6b0ee8915ed5b6de76c71f162b57ac620
Desmond <desmond.bera@gmail.com>
2018-07-04 13:54:04 -0400

    login email and password validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 
 //Load Input Validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 //Load User model
 const User = require('../../models/User');
@@ -67,14 +68,21 @@ router.post('/register', (req, res) => {
 // @access Public
 
 router.post('/login', (req, res) => {
+  const {errors, isValid } = validateLoginInput(req.body);
+  //Check Validation
+  if(!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
   //Find user by email
   User.findOne({email}).then(user => {
       //Check for user
+      errors.email = 'User not found!';
       if(!user) {
-        return res.status(404).json({email: 'User not found'});
+        return res.status(404).json(errors);
       }
 
       //Check password
@@ -97,7 +105,8 @@ router.post('/login', (req, res) => {
             }
           );
           } else {
-            return res.status(400).json({password: 'Password incorrect!'});
+            errors.password = 'Password incorrect!';
+            return res.status(400).json(errors);
           }
         });
     });
