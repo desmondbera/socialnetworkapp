c68c9a18463533d5c74dc7e98e11d20c453280f9
Desmond <desmond.bera@gmail.com>
2018-07-05 22:22:10 -0400

    setup profile field validations

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+//Load validation
+const validateProfileInput = require('../../validation/profile')
+
 //Load Profile Model
 const Profile = require('../../models/Profile');
 
@@ -15,14 +18,45 @@ const User = require('../../models/User');
 
 router.get('/test', (req, res) => res.json({msg: "Profile works"}));
 
+// @route GET api/profile
+// @desc GET CURRENT user profile
+// @access Private
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id})
+      .populate('user', ['name', 'avatar'])
+      .then(profile => {
+        if(!profile) {
+          errors.noprofile = 'There is no profile for this user';
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+
+
 // @route POST api/profile
 // @desc Create or Edit user profile
 // @access Private
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+  const { errors, isValid } = validateProfileInput(req.body);
+  //Check validation
+  if(!isValid) {
+    // Return any errors with 400 status
+    return res.status(400).json(errors);
+  }
+
   //Get fields
   const profileFields = {};
   profileFields.user = req.user.id;
-
   if(req.body.handle) profileFields.handle = req.body.handle;
   if(req.body.company) profileFields.company = req.body.company;
   if(req.body.website) profileFields.website = req.body.website;
@@ -36,11 +70,11 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
   }
   //Social
   profileFields.social = {}
-  if(req.body.youtube) profileFields.social.youtube = req.body.social.youtube;
-  if(req.body.twitter) profileFields.social.twitter = req.body.social.twitter;
-  if(req.body.facebook) profileFields.social.facebook = req.body.social.facebook;
-  if(req.body.linkedin) profileFields.social.linkedin = req.body.social.linkedin;
-  if(req.body.instagram) profileFields.social.instagram = req.body.social.instagram;
+  if(req.body.youtube) profileFields.social.youtube = req.body.youtube;
+  if(req.body.twitter) profileFields.social.twitter = req.body.twitter;
+  if(req.body.facebook) profileFields.social.facebook = req.body.facebook;
+  if(req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+  if(req.body.instagram) profileFields.social.instagram = req.body.instagram;
 
   Profile.findOne({ user: req.user.id })
     .then(profile => {
@@ -50,7 +84,7 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
           { user: req.user.id },
           { $set: profileFields },
           { new: true }
-        ).then(profile => res.json(profile))
+        ).then(profile => res.json(profile));
       } else {
         //Create
 
