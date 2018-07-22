78b7fac2b83ef335e9671febb38679c8d87c3625
Desmond <desmond.bera@gmail.com>
2018-07-21 20:48:23 -0400

    added redux for login action && jwt-decode hookups

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
 
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
 
 //Register User
 export const registerUser = (userData, history) => dispatch => {
@@ -25,9 +27,21 @@ export const loginUser = (userData) => dispatch => {
       localStorage.setItem('jwtToken', token);
       //Set token to Authorization header
       setAuthToken(token);
+      //Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch(setCurrentUser(decoded));
     })
     .catch(err => dispatch({
       type: GET_ERRORS,
       payload: err.response.data
     }))
 };
+
+//Set logged in user
+export const setCurrentUser = (decoded) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
+}
