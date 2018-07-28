5d816d0efb85c6f5cb447267317aa96962d3a7b2
Desmond <desmond.bera@gmail.com>
2018-07-27 21:35:31 -0400

    added NotFound component.

--- client/src/App.js
+++ client/src/App.js
@@ -24,6 +24,7 @@ import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import NotFound from './components/not-found/NotFound';
 
 import './App.css';
 
@@ -77,6 +78,7 @@ class App extends Component {
             <Switch>
               <PrivateRoute exact path="/add-education" component={AddEducation} />
             </Switch>
+              <Route exact path="/not-found" component={NotFound}></Route>
           </div>
           <Footer />
           </div>
