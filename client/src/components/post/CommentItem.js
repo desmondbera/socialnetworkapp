0135549e4d6e353433d054d87821ed98cbc78fcd
Desmond <desmond.bera@gmail.com>
2018-07-30 14:03:14 -0400

    hooked up comment feed under the comment form.

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -4,6 +4,11 @@ import PropTypes from 'prop-types';
 import { deleteComment } from '../../actions/postActions';
 
 class CommentItem extends Component {
+
+  onDeleteClick(postId, commentId) {
+    this.props.deleteComment(postId, commentId)
+  }
+
   render() {
     const { comment, postId, auth } = this.props;
     return(
@@ -24,6 +29,11 @@ class CommentItem extends Component {
             <p className="lead">
               {comment.text}
             </p>
+            {comment.user === auth.user.id ? (
+              <button onClick={ this.onDeleteClick.bind(this, postId, comment._id)} type="button" className="btn btn-danger mr-1">
+                <i className="fas fa-times"/>
+              </button>
+            ) : null }
           </div>
         </div>
       </div>
